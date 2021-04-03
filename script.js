0e80cf47920d71230c3e3e03cf1700e4ccc7d388
Sedarsktian <juan99sebastianmelo@hotmail.com>
2021-04-02 21:22:26 -1000

    changes

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const d = document,
 
     const getAll = async () => {
       try {
-        let res = await fetch("http://localhost:3000/libros"),
+        let res = await fetch("http://localhost:3001/libros"),
           json = await res.json();
 
         if (!res.ok) throw { status: res.status, statusText: res.statusText };
@@ -15,10 +15,10 @@ const d = document,
         console.log(json);
         json.forEach(el => {
           $template.querySelector(".name").textContent = el.nombre;
-          $template.querySelector(".constellation").textContent = el.constelacion;
+          $template.querySelector(".libro").textContent = el.libro;
           $template.querySelector(".edit").dataset.id = el.id;
           $template.querySelector(".edit").dataset.name = el.nombre;
-          $template.querySelector(".edit").dataset.constellation = el.constelacion;
+          $template.querySelector(".edit").dataset.constellation = el.libro;
           $template.querySelector(".delete").dataset.id = el.id;
 
           let $clone = d.importNode($template, true);
@@ -48,10 +48,10 @@ const d = document,
               },
               body: JSON.stringify({
                 nombre: e.target.nombre.value,
-                constelacion: e.target.constelacion.value
+                libro: e.target.libro.value
               })
             },
-              res = await fetch("http://localhost:3000/libros", options),
+              res = await fetch("http://localhost:3001/libros", options),
               json = await res.json();
 
             if (!res.ok) throw { status: res.status, statusText: res.statusText };
@@ -71,10 +71,10 @@ const d = document,
               },
               body: JSON.stringify({
                 nombre: e.target.nombre.value,
-                constelacion: e.target.constelacion.value
+                libro: e.target.libro.value
               })
             },
-              res = await fetch(`http://localhost:3000/libros/${e.target.id.value}`, options),
+              res = await fetch(`http://localhost:3001/libros${e.target.id.value}`, options),
               json = await res.json();
 
             if (!res.ok) throw { status: res.status, statusText: res.statusText };
@@ -90,9 +90,9 @@ const d = document,
 
     d.addEventListener("click", async e => {
       if (e.target.matches(".edit")) {
-        $title.textContent = "Editar Santo";
+        $title.textContent = "Editar Libro";
         $form.nombre.value = e.target.dataset.name;
-        $form.constelacion.value = e.target.dataset.constellation;
+        $form.libro.value = e.target.dataset.libro;
         $form.id.value = e.target.dataset.id;
       }
 
@@ -108,7 +108,7 @@ const d = document,
                 "Content-type": "application/json; charset=utf-8"
               }
             },
-              res = await fetch(`http://localhost:3000/libros/${e.target.dataset.id}`, options),
+              res = await fetch(`http://localhost:3001/libros${e.target.dataset.id}`, options),
               json = await res.json();
 
             if (!res.ok) throw { status: res.status, statusText: res.statusText };
