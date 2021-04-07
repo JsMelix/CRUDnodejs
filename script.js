b34a8be26d8ea235baf376b494b82b19dcffc0b1
Sedarsktian <juan99sebastianmelo@hotmail.com>
2021-04-06 15:02:52 -1000

    deploy

--- script.js
+++ script.js
@@ -108,7 +108,7 @@ const d = document,
                 "Content-type": "application/json; charset=utf-8"
               }
             },
-              res = await fetch("https://crudenode-app.herokuapp.com/libros${e.target.dataset.id}", options),
+            res = await fetch(`https://crudenode-app.herokuapp.com/libros${e.target.id.value}`, options),
               json = await res.json();
 
             if (!res.ok) throw { status: res.status, statusText: res.statusText };
